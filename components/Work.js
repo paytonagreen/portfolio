c6d7bd814dff1cc1e2509da3fd6ae5129701c725
Payton Green <paytonagreen@gmail.com>
2021-05-18 18:55:15 -0500

    work updates

--- components/Work.js
+++ components/Work.js
@@ -7,7 +7,7 @@ import payton from '../public/img/PNG/PGScreen.png';
 import somm from '../public/img/PNG/SOMM-Screengrab.png';
 
 import WorkCard from './WorkCard';
-import WorkStyles from './styles/WorkStyles'
+import WorkStyles from './styles/WorkStyles';
 
 const content = (
   <>
@@ -15,40 +15,40 @@ const content = (
       image={somm}
       link='https://asommforyou.com'
       title='A Somm For You'
-      description='Full Stack Application'
-      frontend="React (Next.js), SWR"
-      backend="Ruby On Rails REST API"
-      styledWith="CSS-In-JS/styled-components"
-      testedWith="Jest/Testing Library"
-    />
-    <WorkCard
-      image={dustin}
-      link='https://dlaltick.paytongreen.com'
-      title='DL Altick Music Co'
-      description='Modern responsive SPA:'
-      frontend="React"
-      styledWith="SASS"
-      testedWith="Jest/Enzyme"
+      description='Full Stack Application:'
+      frontend='Typescript, React (Next.js)'
+      backend='Ruby On Rails REST API'
+      styledWith='styled-components'
+      testedWith='Jest/Testing Library'
     />
     <WorkCard
       image={lisa}
       link='https://lisa-alley.com'
       title='Lisa Alley Store'
       description='Full-Stack E-commerce App:'
-      frontend="React (Next.js) & Apollo Client"
-      backend="Node.js/Express/Prisma ORM, AWS"
-      styledWith="CSS-inJS/styled-components"
-      testedWith="Jest/TestingLibrary"
+      frontend='React (Next.js) & Apollo Client'
+      backend='Node.js/Express/Prisma ORM, AWS'
+      styledWith='styled-components'
+      testedWith='Jest/TestingLibrary'
     />
     <WorkCard
       image={payton}
       link='https://paytongreen.com'
       title='Personal Portfolio'
-      description='Modern, Responsive SPA'
-      frontend="React (Next.js)"
-      styledWith="CSS-in-JS/styled-components"
+      description='Modern, Responsive SPA:'
+      frontend='React (Next.js)'
+      styledWith='styled-components'
     />
-    
+    <WorkCard
+      image={dustin}
+      link='https://dlaltick.paytongreen.com'
+      title='DL Altick Music Co'
+      description='Modern responsive SPA:'
+      frontend='React'
+      styledWith='SASS'
+      testedWith='Jest/Enzyme'
+    />
+
     <h2>...And More On The Way!</h2>
   </>
 );
