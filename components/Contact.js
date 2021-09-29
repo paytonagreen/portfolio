edb996790deffe0db97edf702739c73c8623b4fa
paytonagreen <paytonagreen@gmail.com>
2021-09-29 14:56:05 -0500

    Fix contact links

--- components/Contact.js
+++ components/Contact.js
@@ -1,30 +1,27 @@
-import React from "react";
+import React from 'react';
 import Head from 'next/head';
-import ContactStyles from "./styles/ContactStyles";
-import github from "../public/img/PNG/actualgithub.png";
-import linkedin from "../public/img/PNG/linkedin.png";
-import email from "../public/img/PNG/email.png";
-
+import ContactStyles from './styles/ContactStyles';
+import github from '../public/img/PNG/actualgithub.png';
+import linkedin from '../public/img/PNG/linkedin.png';
+import email from '../public/img/PNG/email.png';
 
 const Contact = () => {
   return (
     <>
-    <Head>
-      <title>
-        Contact | Payton Green
-      </title>
-    </Head>
-    <ContactStyles>
-      <div className="contact">
-        <img src={email} alt="" /> <p>Write Me!</p>
-      </div>
-      <div className="contact">
-        <img src={github} alt="" /> <p>Code With Me!</p>
-      </div>
-      <div className="contact">
-        <img src={linkedin} alt="" /> <p>Hire Me!</p>
-      </div>
-    </ContactStyles>
+      <Head>
+        <title>Contact | Payton Green</title>
+      </Head>
+      <ContactStyles>
+        <a href='mailto: paytonagreen@gmail.com' className='contact'>
+          <img src={email} alt='' /> <p>Write Me!</p>
+        </a>
+        <a href='https://github.com/paytonagreen' className='contact'>
+          <img src={github} alt='' /> <p>Code With Me!</p>
+        </a>
+        <a href='https://linkedin.com/in/paytonagreen' className='contact'>
+          <img src={linkedin} alt='' /> <p>Hire Me!</p>
+        </a>
+      </ContactStyles>
     </>
   );
 };
